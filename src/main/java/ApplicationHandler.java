643cc3782e6c427ce8ee4301c0482e88ed0a7073
weedric <cedi.schreiner@web.de>
2017-12-07 16:10:03 +0100

    - Push Project

---
+++ src/main/java/ApplicationHandler.java
@@ -0,0 +1,15 @@
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+/**
+ * Created by Florian on 13.06.2017.
+ * Legt fest welche Arten von Resourcen an den Server weiter gegeben werden
+ * wenn keine Resourcen angebeben sind == Klasse leer -> Per default: alle
+ * Path: /webapi
+ */
+
+@ApplicationPath("/webapi")
+public class ApplicationHandler extends Application {
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/test")
+public class Test {
+
+    @GET
+    @Path("/get")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "test";
+    }
+}
